bdc940ed8515bc710c22bd3ef0ed73322b237f93
Indrek Mannik <119368421+Inc21@users.noreply.github.com>
2024-04-01 23:39:54 +0100

    Add Caesar Cipher encryption functionality

--- day_008/practice.py
+++ day_008/practice.py
@@ -7,18 +7,18 @@
 # greet()
 
 
-# def greet_with_name(name):
-#     print(f"Hello {name}")
-#     print(f"Welcome to the Caesar Cipher {name}")
-#     print("Let's get started")
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"Welcome to the Caesar Cipher {name}")
+    print("Let's get started")
 
 
 # greet_with_name("Alice")
 
 #  function with more than 1 input
-def greet_with(name, location):
-    print(f"Hello {name}")
-    print(f"What is it like in {location}?")
+# def greet_with(name, location):
+#     print(f"Hello {name}")
+#     print(f"What is it like in {location}?")
 
 
-greet_with("Alice", "Paris")
+# greet_with("Alice", "Paris")
