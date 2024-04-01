13f244a532da54345242a9e65ac7066c93ae1e77
Indrek Mannik <119368421+Inc21@users.noreply.github.com>
2024-04-01 23:01:05 +0100

    Add greet_with function to main.py

---
+++ day_008/practice.py
@@ -0,0 +1,24 @@
+# def greet():
+#     print("Hello")
+#     print("Welcome to the Caesar Cipher")
+#     print("Let's get started")
+
+
+# greet()
+
+
+# def greet_with_name(name):
+#     print(f"Hello {name}")
+#     print(f"Welcome to the Caesar Cipher {name}")
+#     print("Let's get started")
+
+
+# greet_with_name("Alice")
+
+#  function with more than 1 input
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}?")
+
+
+greet_with("Alice", "Paris")
