4d7ff77019186b821eae44f5bffadd6678a0ead1
Indrek Mannik <119368421+Inc21@users.noreply.github.com>
2024-04-08 22:09:32 +0100

    Update dictionary and add nesting examples

--- day_009/practice.py
+++ day_009/practice.py
@@ -1,13 +1,15 @@
 programming_dictionary = {
-    "Bug": "An error in a program that prevents the program from running as expected.",
+    "Bug": "An error in a program that prevents the program from running as \
+      expected.",
     "Function": "A piece of code that you can easily call over and over again."
-    }
+}
 
 #  retrieve an item from a dictionary
 print(programming_dictionary["Bug"])
 
 #  adding new items to dictionary
-programming_dictionary["Loop"] = "The action of doing something over and over again."
+programming_dictionary["Loop"] = "The action of doing something over and over \
+  again."
 print(programming_dictionary)
 
 #  create an empty dictionary
@@ -25,3 +27,43 @@ print(programming_dictionary)
 for key in programming_dictionary:
     print(key)
     print(programming_dictionary[key])
+
+#######################################
+
+#  Nesting
+capitals = {
+  "France": "Paris",
+  "Germany": "Berlin",
+}
+
+#  Nesting a List in a Dictionary
+
+travel_log = {
+  "France": ["Paris", "Lille", "Dijon"],
+  "Germany": ["Berlin", "Hamburg", "Stuttgart"],
+}
+
+#  Nesting Dictionary in a Dictionary
+
+travel_log = {
+  "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12
+             },
+  "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+              "total_visits": 5
+              },
+}
+
+#  Nesting Dictionaries in Lists
+
+travel_log = [
+    {
+        "country": "France",
+        "cities_visited": ["Paris", "Lille", "Dijon"],
+        "total_visits": 12,
+        },
+    {
+        "country": "Germany",
+        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+        "total_visits": 5,
+        },
+]
